56679fd5313bc10653b3b38f9127ba41ac9dee9c
Morten Lied Johansen <morten.lied.johansen@nav.no>
2023-05-09 14:09:37 +0200

    Configure logging

---
+++ src/logging.rs
@@ -0,0 +1,55 @@
+use std::io::Write;
+
+use anyhow::Result;
+use json::JsonValue;
+use log::info;
+use log::kv::{Error, Key, Value};
+
+use crate::{Config, LogFormat};
+
+struct JsonVisitor<'a> {
+    data: &'a mut JsonValue
+}
+
+impl JsonVisitor<'_> {
+    fn new(data: &mut JsonValue) -> JsonVisitor {
+        return JsonVisitor {
+            data,
+        };
+    }
+}
+
+impl<'kvs> log::kv::Visitor<'kvs> for JsonVisitor<'kvs> {
+    fn visit_pair(&mut self, key: Key<'kvs>, value: Value<'kvs>) -> std::result::Result<(), Error> {
+        self.data[key.to_string()] = JsonValue::from(value.to_string());
+        Ok(())
+    }
+}
+
+
+pub fn init_logging(config: &Config) -> Result<()> {
+    match config.log_format {
+        LogFormat::Plain => {
+            env_logger::builder()
+                .filter_level(config.log_level)
+                .init();
+        }
+        LogFormat::Json => {
+            env_logger::builder()
+                .filter_level(config.log_level)
+                .format(|buf, record| {
+                    let mut data = json::object! {
+                        timestamp: buf.timestamp_seconds().to_string(),
+                        level: record.level().to_string(),
+                        message: record.args().to_string(),
+                    };
+                    let mut visitor = JsonVisitor::new(&mut data);
+                    record.key_values().visit(&mut visitor).unwrap();
+                    writeln!(buf, "{}", json::stringify(data))
+                })
+                .init();
+        }
+    };
+    info!("{:?} logger initialized", config.log_format);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,30 @@ use figment::{Figment, providers::Env};
 use figment::providers::Serialized;
 use serde::{Deserialize, Serialize};
 
+mod logging;
+
 #[derive(Debug, Deserialize, Serialize)]
-struct Config {
+pub enum LogFormat {
+    Plain,
+    Json,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Config {
     // The address:port to bind to
     bind_address: String,
+    // Logging format to use
+    log_format: LogFormat,
+    // Log level
+    log_level: log::LevelFilter
 }
 
 impl Default for Config {
     fn default() -> Config {
         Config {
             bind_address: "0.0.0.0:3000".into(),
+            log_format: LogFormat::Plain,
+            log_level: log::LevelFilter::Info,
         }
     }
 }
@@ -31,6 +45,8 @@ fn main() -> Result<()> {
 
 #[tokio::main]
 async fn app(config: Config) -> Result<()> {
+    logging::init_logging(&config)?;
+
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
