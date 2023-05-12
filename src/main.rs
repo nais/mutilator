06e6e7165df4ef6f3ae79af9a300b8634c938738
Morten Lied Johansen <morten.lied.johansen@nav.no>
2023-05-12 16:01:25 +0200

    Add first patch (?)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::path::PathBuf;
+
 use anyhow::Result;
-use figment::{Figment, providers::Env};
-use figment::providers::Serialized;
-use serde::{Deserialize, Serialize};
 use atty::Stream;
+use figment::{Figment, providers::Env};
 use log::info;
+use serde::{Deserialize, Serialize};
 
 mod logging;
 mod web;
@@ -14,6 +14,15 @@ mod aiven_types;
 pub enum LogFormat {
     Plain,
     Json,
+}
+
+impl Default for LogFormat {
+    fn default() -> Self {
+        match atty::is(Stream::Stdout) {
+            true => LogFormat::Plain,
+            false => LogFormat::Json,
+        }
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -26,36 +35,57 @@ pub struct WebConfig {
     private_key_path: Option<PathBuf>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Config {
-    // Logging format to use
-    log_format: LogFormat,
-    // Log level
-    log_level: log::LevelFilter,
-
-    web: WebConfig,
-}
-
-impl Default for Config {
-    fn default() -> Config {
-        let log_format = match atty::is(Stream::Stdout) {
-            true => LogFormat::Plain,
-            false => LogFormat::Json,
-        };
-        Config {
-            log_level: log::LevelFilter::Info,
-            log_format,
-            web: WebConfig {
-                bind_address: "0.0.0.0:3000".into(),
-                certificate_path: None,
-                private_key_path: None,
-            }
+impl Default for WebConfig {
+    fn default() -> Self {
+        WebConfig {
+            bind_address: "0.0.0.0:3000".into(),
+            certificate_path: None,
+            private_key_path: None,
         }
     }
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Tenant {
+    environment: String,
+    name: String,
+}
+
+impl Default for Tenant {
+    fn default() -> Self {
+        Tenant {
+            environment: "local".to_string(),
+            name: "local".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Config {
+    // Logging format to use
+    #[serde(default)]
+    log_format: LogFormat,
+    // Log level
+    #[serde(default = "default_log_level")]
+    log_level: log::LevelFilter,
+    #[serde(default)]
+    web: WebConfig,
+    #[serde(default)]
+    tenant: Tenant,
+
+    // Aiven VPC ID
+    project_vpc_id: String,
+
+    // Cloud location (eq. europe-north1)
+    location: String,
+}
+
+fn default_log_level() -> log::LevelFilter {
+    log::LevelFilter::Info
+}
+
 fn main() -> Result<()> {
-    let config: Config = Figment::from(Serialized::defaults(Config::default()))
+    let config: Config = Figment::new()
         .merge(Env::prefixed("MUTILATOR__").split("__"))
         .extract()?;
     app(config)?;
@@ -67,7 +97,7 @@ async fn app(config: Config) -> Result<()> {
     logging::init_logging(&config)?;
     info!("Configuration loaded: {:?}", config);
 
-    web::start_web_server(config.web).await?;
+    web::start_web_server(config).await?;
 
     Ok(())
 }
--- src/web.rs
+++ src/web.rs
@@ -1,5 +1,7 @@
+use std::sync::Arc;
+
 use anyhow::Result;
-use axum::extract::Json;
+use axum::extract::{Json, State};
 use axum::http::StatusCode;
 use axum::Router;
 use axum::routing::{get, post};
@@ -8,22 +10,28 @@ use kube::core::admission::{AdmissionResponse, AdmissionReview};
 use kube::core::DynamicObject;
 use kube::ResourceExt;
 use log::{info, warn};
+use serde_json::Value;
 
 use crate::aiven_types::aiven_redis::Redis;
-use crate::WebConfig;
+use crate::Config;
 
-pub async fn start_web_server(config: WebConfig) -> Result<()> {
+pub async fn start_web_server(config: Config) -> Result<()> {
+    let certificate_path = config.web.certificate_path.clone();
+    let private_key_path = config.web.private_key_path.clone();
+    let addr = config.web.bind_address.parse().unwrap();
+
+    let state = Arc::new(config);
     let app = Router::new()
         .route("/is_alive", get(|| async { "I'm alive!" }))
         .route("/is_ready", get(|| async { "Ready for action!" }))
         .route("/mutate", post(mutate_handler))
+        .with_state(state)
         ;
 
-    let addr = config.bind_address.parse().unwrap();
-    if config.certificate_path.is_some() && config.private_key_path.is_some() {
+    if certificate_path.is_some() && private_key_path.is_some() {
         let tls_config = RustlsConfig::from_pem_file(
-            config.certificate_path.unwrap(),
-            config.private_key_path.unwrap())
+            certificate_path.unwrap(),
+            private_key_path.unwrap())
             .await?;
         info!("Starting webserver on {} using https", addr);
         axum_server::bind_rustls(addr, tls_config)
@@ -39,7 +47,7 @@ pub async fn start_web_server(config: WebConfig) -> Result<()> {
     Ok(())
 }
 
-async fn mutate_handler(Json(admission_review): Json<AdmissionReview<Redis>>) -> (StatusCode, Json<AdmissionReview<DynamicObject>>) {
+async fn mutate_handler(State(config): State<Arc<Config>>, Json(admission_review): Json<AdmissionReview<Redis>>) -> (StatusCode, Json<AdmissionReview<DynamicObject>>) {
     match admission_review.request {
         None => {
             warn!("No request present in AdmissionReview object");
@@ -51,7 +59,7 @@ async fn mutate_handler(Json(admission_review): Json<AdmissionReview<Redis>>) ->
                 let name = obj.name_any();
                 let namespace = obj.namespace().unwrap();
                 info!(name = name, namespace = namespace; "Processing redis resource");
-                res = match mutate(res.clone(), &obj) {
+                res = match mutate(res.clone(), &obj, &config) {
                     Ok(res) => {
                         info!(name = name, namespace = namespace; "Processing complete");
                         res
@@ -69,6 +77,13 @@ async fn mutate_handler(Json(admission_review): Json<AdmissionReview<Redis>>) ->
     }
 }
 
-fn mutate(res: AdmissionResponse, obj: &Redis) -> Result<AdmissionResponse> {
-    Ok(res)
+fn mutate(res: AdmissionResponse, obj: &Redis, config: &Arc<Config>) -> Result<AdmissionResponse> {
+    let mut patches = Vec::new();
+    if obj.spec.project_vpc_id.is_none() {
+        patches.push(json_patch::PatchOperation::Add(json_patch::AddOperation {
+            path: "/spec/project_vpc_id".to_string(),
+            value: Value::from(config.project_vpc_id.clone()),
+        }))
+    }
+    Ok(res.with_patch(json_patch::Patch(patches))?)
 }
