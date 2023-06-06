533d8280b5a2b10a744036f4295d6d11aa83c4f2
Morten Lied Johansen <morten.lied.johansen@nav.no>
2023-06-06 21:04:37 +0200

    See if we get some opentelemetry traces going

--- src/logging.rs
+++ src/logging.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
+use opentelemetry::sdk::export::trace::stdout;
 use tracing::{info, Level};
-use tracing_subscriber::filter;
+use tracing_subscriber::{filter, Registry};
+use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::prelude::*;
 
 use crate::{Config, LogFormat};
@@ -9,20 +11,24 @@ pub fn init_logging(config: &Config) -> Result<()> {
     let filter = filter::Targets::new()
         .with_default(&config.log_level)
         .with_target("axum::rejection", Level::TRACE);
+    let otel_tracer = stdout::new_pipeline().install_simple();
+    let otel_layer = tracing_opentelemetry::layer().with_tracer(otel_tracer);
     match config.log_format {
         LogFormat::Plain => {
             let fmt_layer = tracing_subscriber::fmt::layer()
                 .compact();
-            tracing_subscriber::registry()
-                .with(filter)
+            Registry::default()
+                .with(otel_layer)
                 .with(fmt_layer)
+                .with(filter)
                 .init();
         }
         LogFormat::Json => {
             let fmt_layer = tracing_subscriber::fmt::layer()
                 .json()
                 .flatten_event(true);
-            tracing_subscriber::registry()
+            Registry::default()
+                .with(otel_layer)
                 .with(fmt_layer)
                 .with(filter)
                 .init();
