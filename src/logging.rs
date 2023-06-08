785654836a00cf846a3263eb1b729fa93cfa07db
Morten Lied Johansen <morten.lied.johansen@nav.no>
2023-06-08 11:43:27 +0200

    Configure OpenTelemetry export to otel endpoint

--- src/logging.rs
+++ src/logging.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
-use opentelemetry::sdk::export::trace::stdout;
+use opentelemetry::sdk::trace::Tracer;
+use opentelemetry_otlp::WithExportConfig;
 use tracing::{info, Level};
+use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::{filter, Registry};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::prelude::*;
@@ -11,8 +13,7 @@ pub fn init_logging(config: &Config) -> Result<()> {
     let filter = filter::Targets::new()
         .with_default(&config.log_level)
         .with_target("axum::rejection", Level::TRACE);
-    let otel_tracer = stdout::new_pipeline().install_simple();
-    let otel_layer = tracing_opentelemetry::layer().with_tracer(otel_tracer);
+    let otel_layer = init_otel()?;
     match config.log_format {
         LogFormat::Plain => {
             let fmt_layer = tracing_subscriber::fmt::layer()
@@ -36,4 +37,16 @@ pub fn init_logging(config: &Config) -> Result<()> {
     };
     info!("{:?} logger initialized", config.log_format);
     Ok(())
+}
+
+fn init_otel() -> Result<OpenTelemetryLayer<Registry, Tracer>> {
+    let otlp_exporter = opentelemetry_otlp::new_exporter()
+        .tonic()
+        .with_env();
+    let otel_tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(otlp_exporter)
+        .install_batch(opentelemetry::runtime::Tokio)?;
+    let otel_layer = tracing_opentelemetry::layer().with_tracer(otel_tracer);
+    Ok(otel_layer)
 }
