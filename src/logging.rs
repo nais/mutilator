0ff1eab54655fe22b8f3fb0e1c5fd89ad9c44bcc
x10an14-nav <christian.chavez@nav.no>
2023-06-09 21:10:35 +0200

    refactor: Run `cargo fmt` after we added `rustfmt.toml`

--- src/logging.rs
+++ src/logging.rs
@@ -1,89 +1,87 @@
-use std::env;
 use anyhow::Result;
-use opentelemetry::KeyValue;
-use opentelemetry::sdk::Resource;
 use opentelemetry::sdk::trace::Tracer;
+use opentelemetry::sdk::Resource;
+use opentelemetry::KeyValue;
 use opentelemetry_otlp::{HasExportConfig, WithExportConfig};
 use opentelemetry_semantic_conventions::resource;
+use std::env;
 use tracing::{info, Level};
 use tracing_opentelemetry::OpenTelemetryLayer;
-use tracing_subscriber::{filter, Registry};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::prelude::*;
+use tracing_subscriber::{filter, Registry};
 
 use crate::settings::{AppConfig, LogFormat};
 
 pub fn init_logging(config: &AppConfig) -> Result<()> {
-    let filter = filter::Targets::new()
-        .with_default(&config.log_level)
-        .with_target("axum::rejection", Level::TRACE);
-    if config.otel_enabled {
-        let (otel_layer, otel_log_msg) = init_otel()?;
-        match config.log_format {
-            LogFormat::Plain => {
-                let fmt_layer = tracing_subscriber::fmt::layer()
-                    .compact();
-                Registry::default()
-                    .with(otel_layer)
-                    .with(fmt_layer)
-                    .with(filter)
-                    .init();
-            }
-            LogFormat::Json => {
-                let fmt_layer = tracing_subscriber::fmt::layer()
-                    .json()
-                    .flatten_event(true);
-                Registry::default()
-                    .with(otel_layer)
-                    .with(fmt_layer)
-                    .with(filter)
-                    .init();
-            }
-        };
-        info!("{:?} logger initialized", config.log_format);
-        info!("{}", otel_log_msg);
-    } else {
-        match config.log_format {
-            LogFormat::Plain => {
-                let fmt_layer = tracing_subscriber::fmt::layer()
-                    .compact();
-                Registry::default()
-                    .with(fmt_layer)
-                    .with(filter)
-                    .init();
-            }
-            LogFormat::Json => {
-                let fmt_layer = tracing_subscriber::fmt::layer()
-                    .json()
-                    .flatten_event(true);
-                Registry::default()
-                    .with(fmt_layer)
-                    .with(filter)
-                    .init();
-            }
-        };
-        info!("{:?} logger initialized", config.log_format);
-    }
-    Ok(())
+	let filter = filter::Targets::new()
+		.with_default(&config.log_level)
+		.with_target("axum::rejection", Level::TRACE);
+	if config.otel_enabled {
+		let (otel_layer, otel_log_msg) = init_otel()?;
+		match config.log_format {
+			LogFormat::Plain => {
+				let fmt_layer = tracing_subscriber::fmt::layer().compact();
+				Registry::default()
+					.with(otel_layer)
+					.with(fmt_layer)
+					.with(filter)
+					.init();
+			},
+			LogFormat::Json => {
+				let fmt_layer = tracing_subscriber::fmt::layer().json().flatten_event(true);
+				Registry::default()
+					.with(otel_layer)
+					.with(fmt_layer)
+					.with(filter)
+					.init();
+			},
+		};
+		info!("{:?} logger initialized", config.log_format);
+		info!("{}", otel_log_msg);
+	} else {
+		match config.log_format {
+			LogFormat::Plain => {
+				let fmt_layer = tracing_subscriber::fmt::layer().compact();
+				Registry::default().with(fmt_layer).with(filter).init();
+			},
+			LogFormat::Json => {
+				let fmt_layer = tracing_subscriber::fmt::layer().json().flatten_event(true);
+				Registry::default().with(fmt_layer).with(filter).init();
+			},
+		};
+		info!("{:?} logger initialized", config.log_format);
+	}
+	Ok(())
 }
 
 fn init_otel() -> Result<(OpenTelemetryLayer<Registry, Tracer>, String)> {
-    let mut otlp_exporter = opentelemetry_otlp::new_exporter()
-        .tonic()
-        .with_env();
-    let otel_log_msg = format!("OpenTelemetry export config: {:?}", otlp_exporter.export_config());
-    let otel_tracer = opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(otlp_exporter)
-        .with_trace_config(
-            opentelemetry::sdk::trace::config().with_resource(Resource::new(vec![
-                KeyValue::new(resource::K8S_CLUSTER_NAME, env::var("NAIS_CLUSTER_NAME").unwrap_or("UNKNOWN_CLUSTER".to_string())),
-                KeyValue::new(resource::K8S_NAMESPACE_NAME, env::var("NAIS_NAMESPACE").unwrap_or("UNKNOWN_NAMESPACE".to_string())),
-                KeyValue::new(resource::K8S_DEPLOYMENT_NAME, env::var("NAIS_APP_NAME").unwrap_or("UNKNOWN_DEPLOYMENT".to_string())),
-                KeyValue::new(resource::SERVICE_NAME, env!("CARGO_BIN_NAME").to_string()),
-            ])),
-        )
-        .install_batch(opentelemetry::runtime::Tokio)?;
-    let otel_layer = tracing_opentelemetry::layer().with_tracer(otel_tracer);
-    Ok((otel_layer, otel_log_msg))
+	let mut otlp_exporter = opentelemetry_otlp::new_exporter().tonic().with_env();
+	let otel_log_msg = format!(
+		"OpenTelemetry export config: {:?}",
+		otlp_exporter.export_config()
+	);
+	let otel_tracer = opentelemetry_otlp::new_pipeline()
+		.tracing()
+		.with_exporter(otlp_exporter)
+		.with_trace_config(
+			opentelemetry::sdk::trace::config().with_resource(Resource::new(vec![
+				KeyValue::new(
+					resource::K8S_CLUSTER_NAME,
+					env::var("NAIS_CLUSTER_NAME").unwrap_or("UNKNOWN_CLUSTER".to_string()),
+				),
+				KeyValue::new(
+					resource::K8S_NAMESPACE_NAME,
+					env::var("NAIS_NAMESPACE").unwrap_or("UNKNOWN_NAMESPACE".to_string()),
+				),
+				KeyValue::new(
+					resource::K8S_DEPLOYMENT_NAME,
+					env::var("NAIS_APP_NAME").unwrap_or("UNKNOWN_DEPLOYMENT".to_string()),
+				),
+				KeyValue::new(resource::SERVICE_NAME, env!("CARGO_BIN_NAME").to_string()),
+			])),
+		)
+		.install_batch(opentelemetry::runtime::Tokio)?;
+	let otel_layer = tracing_opentelemetry::layer().with_tracer(otel_tracer);
+	Ok((otel_layer, otel_log_msg))
 }
--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,24 @@ use tracing::info;
 
 use settings::AppConfig;
 
-mod logging;
-mod web;
 mod aiven_types;
+mod logging;
 mod mutators;
 mod settings;
+mod web;
 
 fn main() -> Result<()> {
-    let config = settings::load_config().map_err(|e| anyhow!(e))?;
-    app(config)?;
-    Ok(())
+	let config = settings::load_config().map_err(|e| anyhow!(e))?;
+	app(config)?;
+	Ok(())
 }
 
 #[tokio::main]
 async fn app(config: AppConfig) -> Result<()> {
-    logging::init_logging(&config)?;
-    info!("Configuration loaded: {:?}", config);
+	logging::init_logging(&config)?;
+	info!("Configuration loaded: {:?}", config);
 
-    web::start_web_server(config).await?;
+	web::start_web_server(config).await?;
 
-    Ok(())
+	Ok(())
 }
