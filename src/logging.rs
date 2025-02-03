b4befb584a2d11b8ea76ca865ab497b1db7ee1de
Morten Lied Johansen <morten.lied.johansen@nav.no>
2025-02-03 11:48:44 +0100

    Adapt to updated opentelemetry libraries

--- src/logging.rs
+++ src/logging.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use opentelemetry::trace::TracerProvider;
 use opentelemetry::KeyValue;
+use opentelemetry_otlp::SpanExporter;
 use opentelemetry_sdk::trace::Tracer;
 use opentelemetry_sdk::Resource;
 use opentelemetry_semantic_conventions::resource;
@@ -42,29 +43,26 @@ fn init_otel(enable: bool) -> Result<Option<OpenTelemetryLayer<Registry, Tracer>
 		return Ok(None);
 	}
 
-	let otlp_exporter = opentelemetry_otlp::new_exporter().tonic();
-	let otel_tracer = opentelemetry_otlp::new_pipeline()
-		.tracing()
-		.with_exporter(otlp_exporter)
-		.with_trace_config(opentelemetry_sdk::trace::Config::default().with_resource(
-			Resource::new(vec![
-				KeyValue::new(
-					resource::K8S_CLUSTER_NAME,
-					env::var("NAIS_CLUSTER_NAME").unwrap_or("UNKNOWN_CLUSTER".to_string()),
-				),
-				KeyValue::new(
-					resource::K8S_NAMESPACE_NAME,
-					env::var("NAIS_NAMESPACE").unwrap_or("UNKNOWN_NAMESPACE".to_string()),
-				),
-				KeyValue::new(
-					resource::K8S_DEPLOYMENT_NAME,
-					env::var("NAIS_APP_NAME").unwrap_or("UNKNOWN_DEPLOYMENT".to_string()),
-				),
-				KeyValue::new(resource::SERVICE_NAME, env!("CARGO_BIN_NAME").to_string()),
-			]),
-		))
-		.install_batch(opentelemetry_sdk::runtime::Tokio)?
-		.tracer("mutilator");
+	let exporter = SpanExporter::builder().with_tonic().build()?;
+	let tracer_provider = opentelemetry_sdk::trace::TracerProvider::builder()
+		.with_resource(Resource::new(vec![
+			KeyValue::new(
+				resource::K8S_CLUSTER_NAME,
+				env::var("NAIS_CLUSTER_NAME").unwrap_or("UNKNOWN_CLUSTER".to_string()),
+			),
+			KeyValue::new(
+				resource::K8S_NAMESPACE_NAME,
+				env::var("NAIS_NAMESPACE").unwrap_or("UNKNOWN_NAMESPACE".to_string()),
+			),
+			KeyValue::new(
+				resource::K8S_DEPLOYMENT_NAME,
+				env::var("NAIS_APP_NAME").unwrap_or("UNKNOWN_DEPLOYMENT".to_string()),
+			),
+			KeyValue::new(resource::SERVICE_NAME, env!("CARGO_BIN_NAME").to_string()),
+		]))
+		.with_batch_exporter(exporter, opentelemetry_sdk::runtime::Tokio)
+		.build();
+	let otel_tracer = tracer_provider.tracer("mutilator");
 	let otel_layer = tracing_opentelemetry::layer().with_tracer(otel_tracer);
 	Ok(Some(otel_layer))
 }
