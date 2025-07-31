d3894abd8d9bcf45c86ced25c8ccf8b5714fa6a3
@x10an14-nav <christian.chavez@nav.no>
2025-07-31 11:31:55 +0200

    refactor(flake): Use treefmt-nix, and nixfmt

--- flake.nix
+++ flake.nix
@@ -5,14 +5,20 @@
     crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, ... }@inputs:
-    inputs.flake-utils.lib.eachDefaultSystem ( system:
+  outputs =
+    { self, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import inputs.nixpkgs {
           inherit system;
@@ -21,9 +27,11 @@
         inherit (pkgs) lib;
 
         # Target musl when building on 64-bit linux
-        buildTarget = {
+        buildTarget =
+          {
             "x86_64-linux" = "x86_64-unknown-linux-musl";
-          }.${system} or (pkgs.stdenv.buildPlatform.rust.rustcTargetSpec);
+          }
+          .${system} or (pkgs.stdenv.buildPlatform.rust.rustcTargetSpec);
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           targets = [
             buildTarget
@@ -45,8 +53,8 @@
             filter =
               path: type:
               let
-                relPath = lib.removePrefix basePath (toString path);
-                includePath = (type == "directory") || lib.any (re: builtins.match re relPath != null) regexes;
+                relPath = pkgs.lib.removePrefix basePath (toString path);
+                includePath = (type == "directory") || pkgs.lib.any (re: builtins.match re relPath != null) regexes;
                 # uncomment to debug:
                 # builtins.trace "${relPath}: ${lib.boolToString includePath}"
               in
@@ -62,55 +70,72 @@
         };
 
         # Compile and cache only cargo dependencies
-        dep-files-filter = [ "Cargo.lock" "Cargo.toml" ".*/Cargo.toml" ];
-        cargo-deps = craneLib.buildDepsOnly (commonArgs // {
-          src = filterSrcWithRegexes dep-files-filter ./.;
-          pname = "${binary-name}-deps";
-        });
+        dep-files-filter = [
+          "Cargo.lock"
+          "Cargo.toml"
+          ".*/Cargo.toml"
+        ];
+        cargo-deps = craneLib.buildDepsOnly (
+          commonArgs
+          // {
+            src = filterSrcWithRegexes dep-files-filter ./.;
+            pname = "${binary-name}-deps";
+          }
+        );
 
         # Compile and cache only workspace code (seperately from 3rc party dependencies)
-        package-file-filter = dep-files-filter
-          ++ [ "src/.+.rs" "src/test_data/.+.yaml" ];
-        cargo-package = craneLib.buildPackage (commonArgs // {
-          inherit cargo-deps;
-          src = filterSrcWithRegexes package-file-filter ./.;
-          pname = binary-name;
-        });
-        dockerTag = if lib.hasAttr "rev" self then
-          "${builtins.toString self.revCount}-${self.shortRev}"
-        else
-          "gitDirty";
-      in {
+        package-file-filter = dep-files-filter ++ [
+          "src/.+.rs"
+          "src/test_data/.+.yaml"
+        ];
+        cargo-package = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargo-deps;
+            src = filterSrcWithRegexes package-file-filter ./.;
+            pname = binary-name;
+          }
+        );
+        dockerTag =
+          if lib.hasAttr "rev" self then
+            "${builtins.toString self.revCount}-${self.shortRev}"
+          else
+            "gitDirty";
+      in
+      {
         checks = {
           # inherit # Comment in when you want tests to run on every new shell
           #   cargo-package
           #   ;
         };
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = (with pkgs; [
-            # rust specific
-            cargo-audit
-            cargo-auditable
-            cargo-cross
-            cargo-deny
-            cargo-outdated
+          nativeBuildInputs =
+            (with pkgs; [
+              # rust specific
+              cargo-audit
+              cargo-auditable
+              cargo-cross
+              cargo-deny
+              cargo-outdated
 
-            # Editor stuffs
-            helix
-            lldb
-            rust-analyzer
+              # Editor stuffs
+              helix
+              lldb
+              rust-analyzer
 
-            # Other tooling
-            earthly
-          ]) ++ [
-            # Packages made in this flake
-            rustToolchain
-            # cargo-package # Comment in when you want tests to run on every new shell
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.Security
-            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux
-            (with pkgs; [ cargo-watch ]); # Currently broken on macOS
+              # Other tooling
+              earthly
+            ])
+            ++ [
+              # Packages made in this flake
+              rustToolchain
+              # cargo-package # Comment in when you want tests to run on every new shell
+            ]
+            ++ lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.Security
+              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            ]
+            ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [ cargo-watch ]); # Currently broken on macOS
 
           shellHook = ''
             ${rustToolchain}/bin/cargo --version
@@ -121,8 +146,7 @@
         packages = {
           rust = cargo-package;
           docker = pkgs.dockerTools.buildImage {
-            name =
-              "europe-north1-docker.pkg.dev/nais-io/nais/images/${binary-name}";
+            name = "europe-north1-docker.pkg.dev/nais-io/nais/images/${binary-name}";
             tag = "v${cargo-details.package.version}_${dockerTag}";
             extraCommands = "mkdir -p data";
             config = {
@@ -133,7 +157,10 @@
         };
         packages.default = cargo-package;
 
-        # Now `nix fmt` works!
-        formatter = pkgs.alejandra;
-      });
+        formatter = inputs.treefmt-nix.lib.mkWrapper pkgs {
+          programs.nixfmt.enable = true;
+          programs.rustfmt.enable = true;
+        };
+      }
+    );
 }
